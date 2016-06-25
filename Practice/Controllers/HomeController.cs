ce08bd2850dad63ad3ff104dfc373d8b6cde2e8c
AbdullahRiaz07 <abdullahriaz9496@gmail.com>
2016-06-25 13:03:41 +0500

    copied and pasted

--- Practice/Controllers/HomeController.cs
+++ Practice/Controllers/HomeController.cs
@@ -16,7 +16,8 @@ namespace Practice.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.";
             return View();
         }
 
